536cec6b0c88f9b231c318a689ee08a2ab46ed35
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-01-09 17:40:28 +0900

    🐛 useAuth -> useExtraHeaders 변경

--- lib/chatting/model/service/socket_service.dart
+++ lib/chatting/model/service/socket_service.dart
@@ -1,19 +1,16 @@
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketService {
-  IO.Socket? socket; // nullable로 선언
+  IO.Socket? socket;
 
-  // 소켓 초기화
   void initializeSocket(String userId) {
     print('userId in initializeSocket : $userId');
 
-    // 기존 소켓이 이미 연결된 경우 연결 해제
     if (socket != null && socket!.connected) {
       print("Existing socket detected. Disconnecting...");
-      disconnect(); // 기존 소켓 정리
+      disconnect();
     }
 
-    // 새 소켓 설정
     Map<String, dynamic> headers = {
       'x-user-id': userId,
     };
@@ -21,20 +18,17 @@ class SocketService {
     socket = IO.io(
       'ws://3.34.17.191:3000',
       IO.OptionBuilder()
-          .setTransports(['websocket']) // 웹소켓 사용 설정
-          .disableAutoConnect() // 자동 연결 비활성화
-          .setAuth(headers) // 헤더 설정
+          .setTransports(['websocket'])
+          .disableAutoConnect()
+          .setExtraHeaders(headers)
           .build(),
     );
 
-    // 소켓 연결
     socket!.connect();
 
-    // 소켓 이벤트 설정
     _setupSocketListeners();
   }
 
-  // 소켓 이벤트 리스너 설정
   void _setupSocketListeners() {
     if (socket == null) {
       print("Socket is not initialized. Unable to set up listeners.");
